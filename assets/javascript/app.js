1c6c9224b5e315ef8f59ba8a87292dea72b1f578
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-01-09 14:56:01 -0800

    index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
 var topics = ["NSX", "GT-R", "Koenigsegg", "McLaren", "Bugatti", "Lamborghini", "Ferrari", "Bentley Flying Spur", "Maserati","Aston Martin", "Porsche 918", "Pagani Huayra", "BMW i8", "Audi RS8", "Mercedes Benz AMG", "Ford GT", "Ford Mustang", "Chevy Camaro", "Pontiac Firebird", "Dodge Charger",  "Toyota Supra",  "Subara WRX"];
+var arrGIF = [];
 
+
+// Original Buttons Created
 function createButtons() {
     $("#buttons-section").empty();
 
@@ -15,10 +18,13 @@ function createButtons() {
 };
 createButtons();
 
+// Display 10 GIFs when above buttons are clicked
 function displayCars () {
 
     var car = $(this).attr("car-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&limit=10&api_key=wAQl5lk5Qi3Yu8pN1WEI95I2fmC2yPuf";
+    var arrGIF = [];
+    var objGIF = {};
 
     $.ajax({
         url: queryURL,
@@ -27,21 +33,40 @@ function displayCars () {
         carInfo = response
         console.log(response);
         $("#gif-section").empty();
+        
         for (j = 0; j < 10; j++) {
+
         var newGIF = $("<img>");
             newGIF.addClass("car-gif");
             newGIF.attr("src", response.data[j].images.fixed_height_still.url);
+            newGIF.attr("pausePlay","still");
+            newGIF.attr("paused", response.data[j].images.fixed_height_still.url);
+            newGIF.attr("animated", response.data[j].images.fixed_height.url);
+            
         var ratingGIF = $("<div>");
             ratingGIF.addClass("rating");
-            ratingGIF.text("Above Rated: " + response.data[j].rating.toUpperCase());
+            ratingGIF.text("Rated: " + response.data[j].rating.toUpperCase());
             $("#gif-section").append(newGIF);
             $("#gif-section").append(ratingGIF);
-        }    
+        }
+    // Play/Stop Image when clicked
+        $(".car-gif").on("click", function(){
+            var state = $(this).attr("pausePlay");
+    
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("animated"));
+                $(this).attr("pausePlay", "playing")
+            } else if (state === "playing"){
+                $(this).attr("src", $(this).attr("paused"));
+                $(this).attr("pausePlay", "still")
+            };
+        })
+    
     });
 };
-
 $(document).on("click", ".cars", displayCars);
 
+// Adds New Car Button from User Input
 $("#add-car").on("click", function(event) {
     event.preventDefault();
     var car = $("#car-input").val().trim();
@@ -49,16 +74,5 @@ $("#add-car").on("click", function(event) {
     createButtons();
 });
 
-var counter = 1
-function playStop () {
-    counter++;
-    console.log(counter);
-    if (counter % 2 === 0) {
-        carInfo.data[j].images.fixed_height_still.url = carInfo.data[j].images.fixed_height.url
-    } 
-    console.log(response.data[j].rating);
-    console.log(response.data[j].url);
-}
-$(document).on("click",".car-gif", playStop)
 
 });
